35b407e53f17156fd59fa6c1b8855c16df369627
yopeace <oldnigel1@hotmail.com>
2020-02-17 02:56:03 +0800

    Added Resource Folder Added ShaunTestScene Scene Added CharacterMovement, CameraFollow, Character, Characters, CharacterStats Script Added Custom Materials Folder Added Test Floor Material Added Test Background Texture

---
+++ Assets/Scripts/ScriptableObjects/CharacterStats.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "new character stat", menuName = "Character/Character Stats", order = 1)]
+public class CharacterStats : ScriptableObject
+{
+    [Header("Character Name")]
+    public string characterName;
+
+    [Header("Stats")]
+    public float characterSpeed;
+    public int characterMaxFruitHold;
+}
---
+++ Assets/Scripts/ScriptableObjects/Characters.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using System.Linq;
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+
+[CreateAssetMenu(fileName = "new characters database", menuName = "Character/Characters Database", order = 2)]
+public class CharactersDatabase : ScriptableObject
+{
+    public List<Character> characters = new List<Character>();
+
+    public void UpdateDatabase()
+    {
+        characters.Clear();
+        Character[] charactersList = Resources.FindObjectsOfTypeAll<Character>();
+        characters.AddRange(charactersList.OrderBy(character => character.characterName));
+
+#if UNITY_EDITOR
+        EditorUtility.SetDirty(this);
+        AssetDatabase.SaveAssets();
+#endif
+    }
+}
+
+#if UNITY_EDITOR
+[CustomEditor(typeof(CharactersDatabase))]
+public class CharactersEditor : Editor
+{
+    public override void OnInspectorGUI()
+    {
+        DrawDefaultInspector();
+        
+        if (GUILayout.Button("Update Character List"))
+        {
+            CharactersDatabase characters = (CharactersDatabase)target;
+            characters.UpdateDatabase();
+        }
+    }
+}
+#endif
