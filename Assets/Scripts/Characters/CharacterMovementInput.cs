8d0271b34a8b24b664ba01b52ef759cc33d9b639
yopeace <oldnigel1@hotmail.com>
2020-02-17 11:39:54 +0800

    Added Movement Scene, Updated CharacterMovement.cs

---
+++ Assets/Scripts/Characters/CharacterMovementInput.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterMovementInput : MonoBehaviour
+{
+    public string horizontalAxisName = "Horizontal";
+    public string verticalAxisName = "Vertical";
+
+    [SerializeField]
+    private float horizontal = 0.0f;
+    [SerializeField]
+    private float vertical = 0.0f;
+
+    public int sendRate = 60;
+    private float sendFrequency = 0.0f;
+    private float sendTimer = 0.0f;
+    public bool moveable = true;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        sendFrequency = 1.0f / sendRate;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        sendTimer += Time.unscaledDeltaTime;
+        horizontal = Input.GetAxis(horizontalAxisName);
+        vertical = Input.GetAxis(verticalAxisName);
+
+        if (sendTimer >= sendFrequency)
+        {
+            //Change this to phase locking call
+            GetComponent<CharacterMovement>().Move(horizontal, vertical);
+            sendTimer = 0.0f;
+        }
+    }
+}
---
+++ Assets/Scripts/Characters/CharacterStats.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "new character stat", menuName = "Character/Character Stats", order = 1)]
+public class CharacterStats : ScriptableObject
+{
+    [Header("Stats")]
+    public float characterSpeed;
+    public int characterMaxFruitHold;
+}
