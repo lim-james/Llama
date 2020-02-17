ec1840ee1b97943bb9a815f978c92bc2a7617739
Sophie <sophiewongsp@gmail.com>
2020-02-17 21:10:04 +0800

    UI
    all basic UI done

---
+++ Assets/Scripts/Screenflow/BlackFade.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BlackFade : MonoBehaviour
+{
+    public RawImage blackImage;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (blackImage.color.a >= 0.0f)
+        {
+            Color currColor = blackImage.color;
+            currColor.a -= 1.2f * Time.deltaTime;
+            blackImage.color = currColor;
+        }
+        else
+            blackImage.enabled = false;
+    }
+}
