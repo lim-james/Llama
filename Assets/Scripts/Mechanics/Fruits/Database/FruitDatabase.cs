677981d8f916a00601b28b577edce4020a359693
yopeace <oldnigel1@hotmail.com>
2020-02-18 13:56:40 +0800

    Added FruitManager.cs, FruitDatabase.cs, FruitInfo.cs Added FruitManager prefab Added ScriptableObjects FruitDatabase, Fruit 1,2,3,4,5 (For Testing)

---
+++ Assets/Scripts/Mechanics/Fruits/Database/FruitDatabase.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+
+[CreateAssetMenu(fileName = "new fruit database", menuName = "Fruit/Fruit Database", order = 1)]
+public class FruitDatabase : ScriptableObject
+{
+    public List<FruitInfo> fruits = new List<FruitInfo>();
+
+    public void UpdateDatabase()
+    {
+        fruits.Clear();
+        FruitInfo[] fruitInfos = Resources.FindObjectsOfTypeAll<FruitInfo>();
+        fruits.AddRange(fruitInfos);
+
+#if UNITY_EDITOR
+        EditorUtility.SetDirty(this);
+        AssetDatabase.SaveAssets();
+#endif
+    }
+}
+
+
+#if UNITY_EDITOR
+[CustomEditor(typeof(FruitDatabase))]
+public class FruitDatabaseEditor : Editor
+{
+    public override void OnInspectorGUI()
+    {
+        DrawDefaultInspector();
+
+        if (GUILayout.Button("Update Fruit Database"))
+        {
+            FruitDatabase fruitDatabase = (FruitDatabase)target;
+            fruitDatabase.UpdateDatabase();
+        }
+
+    }
+}
+#endif
---
+++ Assets/Scripts/Mechanics/Fruits/Database/FruitInfo.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "new fruit info", menuName = "Fruit/Fruit Info", order = 0)]
+public class FruitInfo : ScriptableObject
+{
+    public string fruitName;
+    public GameObject fruitPrefab;
+}
