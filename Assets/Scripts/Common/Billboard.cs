9607c766f3596be8a35ca49db9c9887f2859cfb7
lim-james <jameslimbj@gmail.com>
2020-02-18 18:13:49 +0800

    added basic hud for fruits

---
+++ Assets/Scripts/Common/Billboard.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Billboard : MonoBehaviour
+{
+
+    private Transform camera;
+    private Quaternion rotation;
+
+    private void Start()
+    {
+        camera = Camera.main.transform;
+        rotation = transform.rotation;
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        transform.rotation = camera.rotation * rotation;
+    }
+}
