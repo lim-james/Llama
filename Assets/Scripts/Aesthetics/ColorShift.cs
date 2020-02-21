fd47f9722cc42cdfe7046cad1b2f53157b90352a
Kean <keanwng@gmail.com>
2020-02-21 12:40:04 +0800

    colorshifting for frenzy mode. Out glow, dissolving shaders

---
+++ Assets/Scripts/Aesthetics/ColorShift.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ColorShift : MonoBehaviour
+{
+    public Color[] colors;
+
+    public int currentIndex = 0;
+    private int nextIndex;
+
+    public float changeColorTime = 2.0f;
+    //public float speed = 1;
+    private float lastChange = 0.0f;
+    private float timer = 0.0f;
+
+    [SerializeField]
+    private Image image;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (colors == null || colors.Length < 2)
+            Debug.Log("Need to setup colors array in inspector");
+
+        if (!image)
+            image = GetComponent<Image>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timer += Time.deltaTime;
+
+        if (timer > changeColorTime)
+        {
+            currentIndex = (currentIndex + 1) % colors.Length;
+            nextIndex = (currentIndex + 1) % colors.Length;
+
+            timer = 0.0f;
+        }
+        image.color = Color.Lerp(colors[currentIndex], colors[nextIndex], timer / changeColorTime);
+    }
+}
