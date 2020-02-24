e6508d4313e626a8ead50ec1f9ac01fd20f13096
yopeace <oldnigel1@hotmail.com>
2020-02-24 15:51:35 +0800

    Edited PlayerBaseMaterial's delay duration, Edited Game scene, Added KillZone.cs and Added KillZone to the LayerMask

---
+++ Assets/Scripts/Mechanics/Map/KillZone.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KillZone : MonoBehaviour
+{
+    public ScoreAreaScaling scoringAreas;
+    public GameObject portalPrefab;
+    public float respawnHeightOffset = 2.0f;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.GetComponent<CharacterMovement>())
+            ResetCharacter(other.gameObject);
+        else if (other.GetComponent<Fruit>())
+            DestroyFruit(other.gameObject);
+    }
+
+    public void ResetCharacter(GameObject character)
+    {
+        for (int i = 0; i < scoringAreas.scoringArea.Count; ++i)
+        {
+            if (scoringAreas.scoringArea[i].GetComponent<PlayerBase>().playerID != character.GetComponent<CharacterInput>().controllerID)
+                continue;
+
+            GameObject portal = Instantiate(portalPrefab);
+            portal.transform.position = scoringAreas.scoringArea[i].transform.position + new Vector3(0, respawnHeightOffset, 0);
+            character.transform.position = scoringAreas.scoringArea[i].transform.position + new Vector3(0, respawnHeightOffset, 0);
+            break;
+        }
+
+        //Trigger Inivisibility
+    }
+
+    public void DestroyFruit(GameObject fruit)
+    {
+
+        Destroy(fruit);
+    }
+}
