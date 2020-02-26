dde3f9bef1188421251dcd33ef29c750b606a77f
lim-james <jameslimbj@gmail.com>
2020-02-27 04:26:42 +0800

    JLRP done.

---
+++ Assets/Scripts/Common/Orbit.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+public class Orbit : MonoBehaviour
+{
+
+    [SerializeField]
+    private Transform target;
+    [SerializeField]
+    private float radius = 10.0f;
+    [SerializeField]
+    private float speed = 10.0f;
+
+    private void Update()
+    {
+        float dt = speed * Time.deltaTime;
+
+        transform.RotateAround(target.position, Vector3.up, dt);
+        var desiredPosition = (transform.position - target.position).normalized * radius + target.position;
+        transform.position = Vector3.MoveTowards(transform.position, desiredPosition, dt);
+    }
+}
