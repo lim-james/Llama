c832fdba6ac91b1a715bd4781a360bb4e0d695de
lim-james <jameslimbj@gmail.com>
2020-02-29 03:11:19 +0800

    [Lobby] Colour selection colour

--- Assets/Scripts/Mechanics/Characters/CharacterData.cs
+++ Assets/Scripts/Mechanics/Characters/CharacterData.cs
@@ -1,6 +1,14 @@
-﻿using System.Collections;
+﻿using System;
 using System.Collections.Generic;
 using UnityEngine;
+
+[Serializable]
+public struct MaterialPack
+{
+    public Material primary;
+    public Material secondary;
+}
+
 
 [CreateAssetMenu(fileName = "new character", menuName = "Character/Character", order = 0)]
 public class CharacterData : ScriptableObject
@@ -8,4 +16,6 @@ public class CharacterData : ScriptableObject
     public string characterName;
     public GameObject characterPrefab;
     public CharacterStats stats;
+
+    public List<MaterialPack> teamMaterials;
 }
