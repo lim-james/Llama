6fbcac7648f1555c9a06e33ba76d7a01ca891225
lim-james <jameslimbj@gmail.com>
2020-03-01 03:25:36 +0800

    Layouts can be builts now

---
+++ Assets/Scripts/Mechanics/Layout/LayoutGroup.cs
@@ -0,0 +1,8 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "new layout group", menuName = "Layout Group", order = 0)]
+public class LayoutGroup : ScriptableObject
+{
+    public List<GameObject> group;
+}
