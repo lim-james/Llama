0e69ead04edb6a5c60b1e25e0c019dd84df07ac9
lim-james <jameslimbj@gmail.com>
2020-02-29 15:30:00 +0800

    Added audio manager

---
+++ Assets/AudioPlayer.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(AudioSource))]
+public class AudioPlayer : MonoBehaviour
+{
+    private AudioSource source;
+    
+    private void Awake()
+    {
+        source = GetComponent<AudioSource>();
+    }
+
+    public void Play(AudioClip clip)
+    {
+        source.clip = clip;
+        source.Play();
+    }
+
+}
