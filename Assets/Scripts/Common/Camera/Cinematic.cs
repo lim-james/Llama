170cbe3ab65f29b60c592542d257c4f60a6819f9
lim-james <jameslimbj@gmail.com>
2020-03-02 03:59:34 +0800

    Adding cinematic camera

---
+++ Assets/Scripts/Common/Camera/Cinematic.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+/* Shots
+ * Follow
+ * -    Offset
+ * -    Speed
+ * -    
+ * 
+ */
+
+
+
+public class Shot {
+    public float duration;
+    public List<Transform> targets;
+
+}
+
+
+public class Cinematic : MonoBehaviour
+{
+
+    public enum Mode {
+        Follow,
+        Stationary
+    }
+
+    [SerializeField]
+    private List<Transform> targets;
+
+}
